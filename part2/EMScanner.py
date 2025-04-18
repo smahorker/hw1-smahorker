7054703c9202637cbc59334ac9dcbe7e1fb88253
smahorker <114127699+smahorker@users.noreply.github.com>
2025-04-18 10:42:13 -0700

    Update EMScanner.py

--- part2/EMScanner.py
+++ part2/EMScanner.py
@@ -5,15 +5,19 @@ import argparse
 import sys
 import pdb
 sys.path.append("../part2/")
-from tokens import tokens,Token,Lexeme
-from typing import Callable,List,Tuple,Optional
+from tokens import tokens, Token, Lexeme
+from typing import Callable, List, Tuple, Optional
+
 class ScannerException(Exception):
     pass
+
 class EMScanner:
-    def init(self, tokens: List[Tuple[Token,str,Callable[[Lexeme],Lexeme]]]) -> None:
+    def __init__(self, tokens: List[Tuple[Token, str, Callable[[Lexeme], Lexeme]]]) -> None:
         self.tokens = tokens
-    def input_string(self, input_string:str) -> None:
+    
+    def input_string(self, input_string: str) -> None:
         self.istring = input_string
+    
     def token(self) -> Optional[Lexeme]:
         # Loop until we find a token we can
         # return (or until the string is empty)
@@ -21,7 +25,7 @@ class EMScanner:
             if len(self.istring) == 0:
                 return None
             # For each substring
-            for l in range(len(self.istring),0,-1):
+            for l in range(len(self.istring), 0, -1):
                 matches = []
                 # Check each token
                 for t in tokens:
@@ -30,22 +34,20 @@ class EMScanner:
                     # * second is the possible match
                     # * third is the token action
                     matches.append((t[0],
-                                    re.fullmatch(t[1],self.istring[:l]),
+                                    re.fullmatch(t[1], self.istring[:l]),
                                     t[2]))
                 # Check if there is any token that returned a match
                 # If so break out of the substring loop
                 matches = [m for m in matches if m[1] is not None]
                 if len(matches) > 0:
                     break
-
             if len(matches) == 0:
-                raise ScannerException();
-
+                raise ScannerException()
             # since we are exact matching on the substring, we can
             # arbitrarily take the first match as the longest one            
             longest = matches[0]
             # apply the token action
-            ret = longest[2](Lexeme(longest[0],longest[1][0]))
+            ret = longest[2](Lexeme(longest[0], longest[1][0]))
             # figure how much we need to chop from our input string
             chop = len(ret.value)
             self.istring = self.istring[chop:]
@@ -53,12 +55,12 @@ class EMScanner:
             # return the lexeme
             if ret.token != Token.IGNORE:
                 return ret
-if name == "main":
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('file_name', type=str)
     parser.add_argument('--verbose', '-v', action='store_true')
     args = parser.parse_args()
-
     f = open(args.file_name)    
     f_contents = f.read()
     f.close()
@@ -73,4 +75,4 @@ if name == "main":
         if (verbose):
             print(t)
     end = time()
-    print("time to parse (seconds): ",str(end-start))    
+    print("time to parse (seconds): ", str(end-start))
