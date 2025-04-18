9dfd89b233a1bcc601ff38ca1c8884f000de4e1c
smahorker <114127699+smahorker@users.noreply.github.com>
2025-04-18 10:42:44 -0700

    Update tokens.py

--- part2/tokens.py
+++ part2/tokens.py
@@ -20,19 +20,16 @@ class Token(Enum):
     INT = "INT"
     FLOAT = "FLOAT"
 
-
 class Lexeme:
     def __init__(self, token: Token, value: str) -> None:
         self.token = token
         self.value = value
-
+    
     def __str__(self):
         return "(" + str(self.token) + "," + "\"" + self.value + "\"" + ")"
 
-
 def idy(l: Lexeme) -> Lexeme:
     return l
-
 
 def key(l: Lexeme) -> Lexeme:
     keywords = {
@@ -42,17 +39,14 @@ def key(l: Lexeme) -> Lexeme:
         "int": Token.INT,
         "float": Token.FLOAT
     }
-
     if l.value in keywords:
         return Lexeme(keywords[l.value], l.value)
     return l
-
 
 tokens = [
     (Token.ID, r"[a-zA-Z][a-zA-Z0-9]*", key),
     (Token.NUM, r"[0-9]+(\.[0-9]+)?|\.[0-9]+", idy),
     (Token.HNUM, r"0[xX][0-9a-fA-F]+", idy),
-
     (Token.LPAREN, r"\(", idy),
     (Token.RPAREN, r"\)", idy),
     (Token.LBRACE, r"{", idy),
